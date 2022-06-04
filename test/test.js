0d8907458e12bfba1bf65551f7785f375bb27562
Manu Dwivedi <manu29.d@gmail.com>
2022-06-04 23:54:32 +0530

    Add test for handling null/undefined/empty values as arguments

--- test/test.js
+++ test/test.js
@@ -2,6 +2,24 @@ import assert from 'assert';
 import Add from '../Add';
 
 describe('#Add', () => {
+  describe('when no argument is provided', () => {
+    it('should return 0', () => {
+      assert.equal(Add(), 0);
+    });
+  });
+
+  describe('when null is provided as argument', () => {
+    it('should return 0', () => {
+      assert.equal(Add(null), 0);
+    });
+  });
+
+  describe('when undefined is provided as argument', () => {
+    it('should return 0', () => {
+      assert.equal(Add(undefined), 0);
+    });
+  });
+
   describe('when an empty string is provided', () => {
     it('should return 0', () => {
       assert.equal(Add(""), 0);
