2eeaedeb5a9bf267dd847b421e301569f3ab87db
Manu Dwivedi <manu29.d@gmail.com>
2022-06-08 21:02:22 +0530

    Add support for custom delimiters

--- Add.js
+++ Add.js
@@ -1,12 +1,14 @@
-
 const Add = (str) => {
   if (!str || !str.length) return 0;
   if (str.length === 1) return parseInt(str);
 
-  const DELIMITERS = ',\n';
-  const re = new RegExp(`[${DELIMITERS}]{2,}`);
+  let customDelimiter = str.match(/^\/\/(.)\\n/);
+  customDelimiter = customDelimiter && customDelimiter[1];
+  const DELIMITERS = `${ customDelimiter || ',' }\n`;
+  const re = new RegExp(`${ customDelimiter ? '\/\/.\\n' : '' }[${DELIMITERS}]{2,}`);
 
   // contiguous occurence of delimiters marks an invalid input
+  // based on commucation received over email
   const isInputInvalid = str.match(re);
 
   if (isInputInvalid) return 0;
--- test/test.js
+++ test/test.js
@@ -2,51 +2,61 @@ import assert from 'assert';
 import Add from '../Add';
 
 describe('#Add', () => {
-  describe('when no argument is provided', () => {
-    it('should return 0', () => {
-      assert.equal(Add(), 0);
+  describe('invalid input', () => {
+    describe('when no argument is provided', () => {
+      it('should return 0', () => {
+        assert.equal(Add(), 0);
+      });
+    });
+
+    describe('when null is provided as argument', () => {
+      it('should return 0', () => {
+        assert.equal(Add(null), 0);
+      });
+    });
+
+    describe('when undefined is provided as argument', () => {
+      it('should return 0', () => {
+        assert.equal(Add(undefined), 0);
+      });
+    });
+
+    describe('when an empty string is provided', () => {
+      it('should return 0', () => {
+        assert.equal(Add(""), 0);
+      });
+    });
+
+    describe('when contiguous delimiters are provided', () => {
+      it('should return 0', () => {
+        assert.equal(Add('1,,'), 0);
+      });
     });
   });
 
-  describe('when null is provided as argument', () => {
-    it('should return 0', () => {
-      assert.equal(Add(null), 0);
+  describe('valid input', () => {
+    describe('when only 1 number is provided', () => {
+      it('should return the number', () => {
+        assert.equal(Add("4"), 4);
+      });
     });
-  });
 
-  describe('when undefined is provided as argument', () => {
-    it('should return 0', () => {
-      assert.equal(Add(undefined), 0);
+    describe('when 2 numbers are provided', () => {
+      it('should return the sum of the numbers', () => {
+        assert.equal(Add("1,4"), 5);
+      });
     });
-  });
 
-  describe('when an empty string is provided', () => {
-    it('should return 0', () => {
-      assert.equal(Add(""), 0);
+    describe('when N numbers are provided', () => {
+      it('should return the sum of the numbers', () => {
+        assert.equal(Add("1,2,3,4"), 10);
+      });
     });
-  });
 
-  describe('when the input is invalid', () => {
-    it('should return 0', () => {
-      assert.equal(Add('1,,'), 0);
-    });
-  });
-
-  describe('when only 1 number is provided', () => {
-    it('should return the number', () => {
-      assert.equal(Add("4"), 4);
-    });
-  });
-
-  describe('when 2 numbers are provided', () => {
-    it('should return the sum of the numbers', () => {
-      assert.equal(Add("1,4"), 5);
-    });
-  });
-
-  describe('when N numbers are provided', () => {
-    it('should return the sum of the numbers', () => {
-      assert.equal(Add("1,2,3,4"), 10);
+    describe('when a custom delimiter is provided', () => {
+      it('should return the sum of the numbers', () => {
+        assert.equal(Add("//;\n1;2;3;4"), 10);
+      });
     });
   });
 });
